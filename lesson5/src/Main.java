0b8452c292653a348a02867f06d81d6ed58b199c
Alina Sazonova <alinkasazzz@gmail.com>
2020-04-04 21:45:09 +0300

    lessson5

---
+++ lesson5/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+         final int size = 1000000;
+        float[] arr = new float[size];
+        for (int i = 0;i<1000000; i++){
+            arr[i] = 1;
+            System.out.println(arr[i]);
+        }
+
+    }
+    long a = System.currentTimeMillis();
+}
